1180d0bb8d96be56507f09022ffa0d94a7001e19
Minhyung Cho <dup33333@hufs.ac.kr>
2023-11-04 17:26:40 +0900

    feat: main page Structure

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,7 +1,27 @@
+import { useSetScreenSize } from '../../setScreenHeight';
 import * as styles from './mainStyle'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import backgroundImage from '../../assets/mainBackground.png'
+import plant from '../../assets/plant.png'
 
 export default function MainPage(){
+    useSetScreenSize();
     return(
-        <div>main</div>
+        <styles.MainContainer style={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: 'cover',
+        }}>
+            <styles.HeaderContainer>
+                <styles.TreeInf>새싹이의 지구 LV.1</styles.TreeInf>
+                <FontAwesomeIcon icon={faBars} style={{color: "#000000"}}/>
+            </styles.HeaderContainer>
+            <styles.WateringInf>💧 물(1/10)</styles.WateringInf>
+            <styles.Watering>오늘의 물주기</styles.Watering>
+            <styles.Plant src={plant}/>
+            <styles.Quotes>내일 지구가 멸망하더라도<br/>나는 오늘 한 그루의 사과나무를 심겠다.</styles.Quotes>
+            <styles.Quotes>109,282명이 함께 하고 있어요.</styles.Quotes>
+
+        </styles.MainContainer>
     );
 }
