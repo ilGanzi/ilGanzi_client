51fc11dde96340b5c0da10035fefdc7951cba72a
Minhyung Cho <dup33333@hufs.ac.kr>
2023-11-05 10:52:48 +0900

    Design: onborn page design fix

--- src/pages/onborn/onborn.jsx
+++ src/pages/onborn/onborn.jsx
@@ -1,19 +1,21 @@
 import { useSetScreenSize } from "../../setScreenHeight";
 import * as styles from './onbornStyle';
 import Lottie from "lottie-react";
-import forest from "../../assets/forestLottie.json";
 import hand from "../../assets/handLottie.json";
+import levelImg from "../../assets/level1.png";
 
 export default function OnBorn() {
     useSetScreenSize();
+
     return (
         <styles.Container>
             <styles.LottieWrapper>
             <Lottie animationData={hand} />
             </styles.LottieWrapper>
             <styles.InfWrapper>
-                <styles.PlantInf>새싹이</styles.PlantInf>
-                <styles.PlantInf>Lv.1</styles.PlantInf>
+                <styles.LevelImg src={levelImg}/>
+                <styles.PlantInf>🎉 새싹이의 탄생 🎉</styles.PlantInf>
+                <styles.GotoMain>물 주러 가기</styles.GotoMain>
             </styles.InfWrapper>
         </styles.Container>
     );
