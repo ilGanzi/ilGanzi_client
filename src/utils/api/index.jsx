de764dadb9d25a3fc722f8fc34660a4508b0e581
Minhyung Cho <dup33333@hufs.ac.kr>
2023-11-09 00:25:22 +0900

    fix: userapi class apicall fix

--- src/utils/api/index.jsx
+++ src/utils/api/index.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { login, setAccessToken } from "../store/reducer/user";
 import { useNavigate } from "react-router-dom";
-import { noAuthApi } from "../interceptor/axiosInterceptor";
+import { apicall } from "../interceptor/axiosInterceptor";
+import { useDispatch, useSelector } from "react-redux";
 
 class UserApi {
     static async postLogin(email,pw){
@@ -10,7 +11,7 @@ class UserApi {
                 email: email,
                 password: pw,
             };
-            const response = await noAuthApi.post(`https://ilganziback-lvwun.run.goorm.site/api/accounts/auth/`,loginData);
+            const response = await apicall.post(`/api/accounts/auth/`,loginData);
             const accessToken = response.data.token.access
             axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
             console.log("login success");
@@ -19,17 +20,19 @@ class UserApi {
             console.error(error)
         }
     };
-    static async postRegister(email,pw) {
+
+    static async postRegister(email,pw,phonenum) {
         try{
             const registerData = {
                 email: email,
-                password: pw
+                password: pw,
+                phonenum: phonenum,
             };
-            const response = await axios.get(`/api/accounts/register/`, registerData)
+            const response = await apicall.get(`/api/accounts/register/`, registerData)
         } catch(error){
             console.error(error);
         }
-    }
+    };
 
     static async deleteLogout(email,pw){
         try{
@@ -37,6 +40,12 @@ class UserApi {
                 email: email,
                 password: pw
             };
+            const response = await apicall.delete(`/api/accounts/auth/`)
+            const dispatch = useDispatch();
+            dispatch(login({
+                isAuthorized: false,
+                refreshToken: "",
+            }));
         } catch(error){
             console.error(error);
         }
@@ -44,14 +53,42 @@ class UserApi {
 
     static async postRefresh() {
         try{
+            const authData = useSelector((state) => state.user)
+            const refreshToken = userData.value.refreshToken        
+ 
             const tokenData = {
-                refresh: tokenData
+                refresh: refreshToken
             };
-            const response = await axios.post(`/api/accounts/auth/refresh/`,tokenData)
+            const response = await axios.post(`/api/accounts/auth/refresh/`,tokenData);
+            const accessToken = response.data.access;
+
+            axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+
         } catch(error){
             console.error(error);
         }
-    }
+    };
+
+
+    static async getUser() {
+        try{
+            const response = await apicall.post(`/api/accounts/user/`);
+            return response.data
+        } catch(error){
+            console.error(error);
+        }
+    };
+
+    static async postWatering() {
+        try{
+            const response = await apicall.post(`/api/accounts/update/watering/`);
+            return
+        } catch(error){
+            console.error(error);
+        }
+    };
+
+    
 }
 
 export default UserApi;
